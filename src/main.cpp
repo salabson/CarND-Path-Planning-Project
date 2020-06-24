683ea2575261590657e3c9c93fcd3293278d5c2b
salabson4@yahoo.co.uk <salabson4@yahoo.co.uk>
2020-06-24 11:40:55 +0100

    Feat: make the car to drive on the center of the middle lane

--- src/main.cpp
+++ src/main.cpp
@@ -100,11 +100,13 @@ int main() {
            */
            double dist_inc = 0.5;
            for (int i = 0; i < 50; ++i) {
+            //next longitudinal distance
             double next_s = car_s + i+1*dist_inc;
-            double next_d = lane_width+lane_width/2;
-            
-            next_x_vals.push_back(car_x+(dist_inc*i)*cos(deg2rad(car_yaw)));
-            next_y_vals.push_back(car_y+(dist_inc*i)*sin(deg2rad(car_yaw)));
+            // middle of the center lane
+            double next_d =6;
+            vector<double> xy = getXY(next_s,next_d,map_waypoints_s, map_waypoints_x,map_waypoints_y );
+            next_x_vals.push_back(xy[0]);
+            next_y_vals.push_back(xy[1]);
            }
 
           msgJson["next_x"] = next_x_vals;
