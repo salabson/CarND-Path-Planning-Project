cd18e871cf70ee36b0f13ef85f7f9e3c470057be
salabson4@yahoo.co.uk <salabson4@yahoo.co.uk>
2020-06-25 12:21:08 +0100

    Feat: use sensor fusion to track other cars and take action

--- src/main.cpp
+++ src/main.cpp
@@ -104,7 +104,38 @@ int main() {
 
 
 
+          // sensor fusion
+          if(prev_size > 0)
+          {
+	     car_s = end_path_s;
+          }
           
+          bool too_close = false;
+
+          // find ref_V to use
+          for(int i=0; i<sensor_fusion.size();i++)
+	  {
+            // car is in my lane
+            float d = sensor_fusion[i][6];
+            if(d<(2+4*lane+2) && d>(2+4*lane-2))
+            {
+              double vx = sensor_fusion[i][3];
+              double vy = sensor_fusion[i][4];
+              double check_speed = sqrt(vx*vx+vy*vy);
+              double check_car_s = sensor_fusion[i][5];
+
+	     check_car_s+=((double)prev_size*.02*check_speed);
+             // check s value greaater than mine and s gap
+             if((check_car_s > car_s)&&((check_car_s-car_s) < 30))          
+                // Do some logic here, lower ref vel so we dont crash into the car in front of us,
+                // could also flag to change lane
+                ref_vel = 29.5; //mph
+                too_close = true;
+	     {
+
+	     }
+            }
+          }
 
          // Create a list of widely spaced (x,y) waypoints, evenly spaced at 30m
          // later we will interpolate these waypoint with a spline and fill it in with more points that control speed
